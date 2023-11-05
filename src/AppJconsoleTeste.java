c977c9208c5314e95be876038acbb9ac90270eb6
EdsonRego <edsonxrego@gmail.com>
2023-11-05 14:03:18 -0300

    Ada Tech - Java Basic

---
+++ src/AppJconsoleTeste.java
@@ -0,0 +1,6 @@
+public class AppJconsoleTeste {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        scanner.next();
+    }
+}
